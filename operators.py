db2f7c485080a9cc219e5801dd5392dcb1ff0777
IASpace <ia.space@zhdk.ch>
2022-05-31 19:54:42 +0200

    first implementation of frame meta data protocol

--- operators.py
+++ operators.py
@@ -3,7 +3,8 @@ import bgl
 import gpu
 from gpu_extras.presets import draw_texture_2d
 
-
+import json
+from dataclasses import dataclass
 import logging
 
 logger = logging.getLogger(__name__)
@@ -16,11 +17,41 @@ except ModuleNotFoundError as ex:
 import uuid
 
 #dictionary to store the references to
+db_frameHandle = {} # the draw handler 
 db_drawHandle = {} # the draw handler 
 db_spoutInstances = {} # the spout instance
 
+
+@dataclass
+class FrameMetDataBuffer:
+    content: str = ""
+
+def frame_metadata(name, frame_metadata_buffer):
+    def handler(scene, depsgraph):
+        camera = scene.objects.get(name)
+        if not camera:
+            return
+
+        container = {}
+        container['version'] = 1 
+        container['frame'] = scene.frame_current
+        container['ts'] = 0
+
+        extrinsic = {}
+        extrinsic['pos'] = [camera.location[0], camera.location[1], camera.location[2]]
+        extrinsic['quat'] = [camera.rotation_quaternion[0], camera.rotation_quaternion[1], camera.rotation_quaternion[2], camera.rotation_quaternion[3]]
+        extrinsic['matrix'] = [camera.matrix_world[0][0], camera.matrix_world[0][1], camera.matrix_world[0][2], camera.matrix_world[0][3], camera.matrix_world[1][0], camera.matrix_world[1][1], camera.matrix_world[1][2], camera.matrix_world[1][3], camera.matrix_world[2][0], camera.matrix_world[2][1], camera.matrix_world[2][2], camera.matrix_world[2][3], camera.matrix_world[3][0], camera.matrix_world[3][1], camera.matrix_world[3][2], camera.matrix_world[3][3]]
+
+        container['extrinsic'] = extrinsic
+
+        fraMeDaPro = {}
+        fraMeDaPro['freMeDaPro'] = container
+        frame_metadata_buffer.content = json.dumps(fraMeDaPro)
+    return handler
+
+
 # function for the draw handler to capture the texture from the perspective of the camera
-def texshare_capture(self, context, camera, object, space, region, scene, layer, offscreen, spoutSender, showPreview):
+def texshare_capture(self, context, camera, object, space, region, scene, layer, offscreen, spoutSender, showPreview, frame_metadata_buffer):
     dWIDTH = camera.texshare.capture_width
     dHEIGHT = camera.texshare.capture_height
     applyCM = camera.texshare.applyColorManagmentSettings
@@ -44,6 +75,10 @@ def texshare_capture(self, context, camera, object, space, region, scene, layer,
     if showPreview:
         bgl.glDisable(bgl.GL_DEPTH_TEST)
         draw_texture_2d(offscreen.color_texture, (10, 10), dWIDTH / 4, dHEIGHT / 4)
+
+    buffer =  frame_metadata_buffer.content
+
+    spoutSender.writeMemoryBuffer(camera.name, buffer, len(buffer))
 
     spoutSender.sendTexture(offscreen.color_texture, bgl.GL_TEXTURE_2D, dWIDTH, dHEIGHT, True, 0)
     spoutSender.setFrameSync(camera.name)
@@ -71,6 +106,7 @@ def texshare_main(self, context):
         spoutSender = SpoutGL.SpoutSender()
         spoutSender.setSenderName(context.camera.name)       
         
+        spoutSender.createMemoryBuffer(context.camera.name, 1024)
         # create a off screen renderer
         offscreen = gpu.types.GPUOffScreen(dWIDTH, dHEIGHT)
 
@@ -97,19 +133,26 @@ def texshare_main(self, context):
         bpy.context.window.scene = myCurrentScene
         bpy.context.window.view_layer = myCurrentLayer
 
+        frame_metadata_buffer = FrameMetDataBuffer("test")
+
         # collect all the arguments to pass to the draw handler
-        args = (self, context, context.camera, context.object, mySpace, myRegion, myScene, myLayer, offscreen, spoutSender, guivars.preview)
+        args = (self, context, context.camera, context.object, mySpace, myRegion, myScene, myLayer, offscreen, spoutSender, guivars.preview, frame_metadata_buffer)
         
+        frameHandler = frame_metadata(context.camera.name, frame_metadata_buffer)
+        bpy.app.handlers.depsgraph_update_post.append(frameHandler)
+
         # instantiate the draw handler, 
         # using the texshare_capture function defined above
         drawhandle = bpy.types.SpaceView3D.draw_handler_add(texshare_capture, args, 'WINDOW', 'POST_PIXEL')
         
         # store the references inside the db-dicts
+        db_frameHandle[dbID] = frameHandler
         db_drawHandle[dbID] = drawhandle
         db_spoutInstances[dbID] = spoutSender
         
     # if streaming has been disabled and my ID is still stored in the db
     if context.camera.texshare.enable == 0 and dbID in db_drawHandle:
+        bpy.app.handlers.depsgraph_update_post.remove(db_frameHandle[dbID])
         bpy.types.SpaceView3D.draw_handler_remove(db_drawHandle[dbID], 'WINDOW')
         db_spoutInstances[dbID].releaseSender()
         #removing my ID
